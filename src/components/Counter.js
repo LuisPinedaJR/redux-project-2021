ce5a4c4712186f58e85e645fa41f79f864d53d68
Luis Pineda <lspnd00@gmail.com>
2021-09-15 01:50:43 -0400

    split code intotheir own folders/slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { counterActions } from '../store/index'
+import { counterActions } from '../store/counter'
 
 import classes from './Counter.module.css'
 
 const Counter = () => {
   const dispatch = useDispatch()
-  const counter = useSelector(state => state.counter)
-  const show = useSelector(state => state.showCounter)
+  const counter = useSelector(state => state.counter.counter)
+  const show = useSelector(state => state.counter.showCounter)
 
   const incrementHandler = () => {
     dispatch(counterActions.increment())
   }
 
-  const increasebyFive = () => {
+  const increaseByFive = () => {
     dispatch(counterActions.increaseByFive(5))
   }
 
@@ -30,7 +30,7 @@ const Counter = () => {
       {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increasebyFive}>Increment by 5</button>
+        <button onClick={increaseByFive}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
 
